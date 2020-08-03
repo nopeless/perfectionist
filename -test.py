18d53d71c5776c6eb04f70e5caf17ad03c3654b3
nopeless <38830903+nopeless@users.noreply.github.com>
2020-08-03 20:55:12 +0900

    minor

---
+++ -test.py
@@ -0,0 +1,24 @@
+import time
+
+
+
+
+
+
+
+
+
+start = time.time()
+
+a=0
+for i in range(10):
+	# pass
+	sorted(list(range(999999,-1,-1)))
+
+print(-start+(start:=time.time()))
+
+for i in range(10):
+	sorted(list(range(1000000)))
+
+print(-start+(start:=time.time()))
+print("-"*100)
